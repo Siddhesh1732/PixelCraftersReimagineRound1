42b0f7362653bbe5e6a3f73bbbc2163e8ac147e3
Siddhesh1732 <112747980+Siddhesh1732@users.noreply.github.com>
2024-07-07 22:00:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+// Navigation
+window.addEventListener("scroll",function(){
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+const navigation = document.querySelector('nav')
+document.querySelector('.toggle').onclick = function(){
+    this.classList.toggle('active');
+    navigation.classList.toggle('active');
+  
+}
+
+// Slideshow
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";  
+}
+slideIndex++;
+if (slideIndex > slides.length) {slideIndex = 1}    
+for (i = 0; i < dots.length; i++) {
+dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// Philosophy
+
+const dynamicText = document.querySelector("h1 span");
+const words = ["a world created for the young","the heart of choices","a place where connections are cherished","Maruti Suzuki Arena"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0,charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+    if(!isDeleting && charIndex < currentWord.length){
+        charIndex++;
+        setTimeout(typeEffect, 50);
+    }else if(isDeleting && charIndex > 0){
+        charIndex--;
+        setTimeout(typeEffect, 25);
+    }else{
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex +1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+typeEffect();
+
+// Car Video
+
+const videoSection = document.querySelector('.car-video');
+const options = {
+    threshold: 0.5 // Trigger at 50% of the section being visible
+};
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            videoSection.classList.add('active');
+        } else {
+            videoSection.classList.remove('active');
+        }
+    });
+}, options);
+
+observer.observe(videoSection);
+
+// car section 
+
+const slider = document.querySelector(".items");
+		const slides = document.querySelectorAll(".item");
+		const button = document.querySelectorAll(".button");
+
+		let current = 0;
+		let prev = 4;
+		let next = 1;
+
+		for (let i = 0; i < button.length; i++) {
+			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+		}
+
+		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+		const gotoNum = number => {
+			current = number;
+			prev = current - 1;
+			next = current + 1;
+
+			for (let i = 0; i < slides.length; i++) {
+				slides[i].classList.remove("active");
+				slides[i].classList.remove("prev");
+				slides[i].classList.remove("next");
+			}
+
+			if (next == 5) {
+				next = 0;
+			}
+
+			if (prev == -1) {
+				prev = 4;
+			}
+
+			slides[current].classList.add("active");
+			slides[prev].classList.add("prev");
+			slides[next].classList.add("next");
+		}
